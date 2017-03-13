1d6696ddaa86c5c395715b60a2cf6ca1d5d90949
sergiy <sergiy@sergiy-Lenovo-IdeaPad-Y510P>
2017-03-13 19:27:13 +0200

    fsd

--- CookBook3/src/main/java/ua/controller/user/IndexController.java
+++ CookBook3/src/main/java/ua/controller/user/IndexController.java
@@ -1,13 +1,41 @@
 package ua.controller.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ua.entity.User;
+import ua.service.UserService;
 
 @Controller
 public class IndexController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping("/")
 	public String index(){
 		return "user-index";
+	}
+	
+	@GetMapping("/registration")
+	public String registration(Model model){
+		model.addAttribute("user", new User());
+		return "user-registration";
+	}
+	
+	@PostMapping("/registration")
+	public String save(@ModelAttribute("user") User user){
+		userService.save(user);
+		return "redirect:/login";
+	}
+	
+	@GetMapping("/login")
+	public String login(){
+		return "user-login";
 	}
 }
