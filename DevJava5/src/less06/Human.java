9b3b77fb74f1aee46c6dc9fa368da19dbe27dccd
sergiy <sergiy@sergiy-Lenovo-IdeaPad-Y510P>
2017-05-31 20:23:43 +0300

    sfd

---
+++ DevJava5/src/less06/Human.java
@@ -0,0 +1,27 @@
+package less06;
+
+public class Human {
+
+	int age;
+	
+	String name = "";
+	
+	Human(){
+		printData();
+		System.out.println("Hello");
+	}
+	
+	Human(String name, int age){
+		this.name = name;
+		this.age = age;
+		System.out.println(this);
+	}
+	
+	void run(){
+		System.out.println(this.name+": go-go-go!!!");
+	}
+	
+	void printData(){
+		System.out.println(this.name+" "+age);
+	}
+}
---
+++ DevJava5/src/less06/Main.java
@@ -0,0 +1,14 @@
+package less06;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Human human = new Human();
+		human.age = 18;
+		human.name = "Ivan";
+		human.printData();
+		human.run();
+		Human human2 = new Human("Mykola", 25);
+		System.out.println(human2);
+	}
+}
