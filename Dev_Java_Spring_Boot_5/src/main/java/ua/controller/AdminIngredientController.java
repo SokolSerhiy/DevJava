64527dac0d76970f4134e886dc83035cac73ef3e
Sokol Sergiy <sokol.serhij@gmail.com>
2017-08-18 19:38:56 +0300

    Signed-off-by: Sokol Sergiy <sokol.serhij@gmail.com>

--- Dev_Java_Spring_Boot_5/src/main/java/ua/controller/AdminIngredientController.java
+++ Dev_Java_Spring_Boot_5/src/main/java/ua/controller/AdminIngredientController.java
@@ -4,16 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import ua.entity.Ingredient;
 import ua.service.IngredientService;
 
 @Controller
 @RequestMapping("/admin/ingredient")
+@SessionAttributes("ingredient")
 public class AdminIngredientController {
 
 	private final IngredientService service;
@@ -21,6 +24,11 @@ public class AdminIngredientController {
 	@Autowired
 	public AdminIngredientController(IngredientService service) {
 		this.service = service;
+	}
+	
+	@ModelAttribute("ingredient")
+	public Ingredient getForm() {
+		return new Ingredient();
 	}
 	
 	@GetMapping
@@ -36,8 +44,20 @@ public class AdminIngredientController {
 	}
 	
 	@PostMapping
-	public String save(@RequestParam String name) {
-		service.save(new Ingredient(name));
+	public String save(@ModelAttribute("ingredient") Ingredient ingredient, SessionStatus status) {
+		service.save(ingredient);
+		return cancel(status);
+	}
+	
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable Integer id, Model model) {
+		model.addAttribute("ingredient", service.findOne(id));
+		return show(model);
+	}
+	
+	@GetMapping("/cancel")
+	public String cancel(SessionStatus status) {
+		status.setComplete();
 		return "redirect:/admin/ingredient";
 	}
 }
