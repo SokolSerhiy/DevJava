6e5675335f6ddece2fca47ded9c0a59434943fec
Sokol Sergiy <sokol.serhij@gmail.com>
2017-09-01 17:32:44 +0300

    Signed-off-by: Sokol Sergiy <sokol.serhij@gmail.com>

--- Dev_Java_Spring_Boot_3/src/main/java/ua/entity/Brand.java
+++ Dev_Java_Spring_Boot_3/src/main/java/ua/entity/Brand.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="brand")
+@Table(name="brand", indexes=@Index(columnList = "name", unique=true))
 public class Brand extends AbstractEntityName{
 
 	@OneToMany(mappedBy="brand")
